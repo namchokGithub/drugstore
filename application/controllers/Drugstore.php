93f756df38d103661f56cbf2b037c94030116c08
namchokGithub <pech4751@gmail.com>
2019-12-11 09:15:44 +0700

    add

--- application/controllers/Drugstore.php
+++ application/controllers/Drugstore.php
@@ -112,16 +112,3 @@ class Drugstore extends CI_Controller {
 
     }
 }
-
-?>
-<!-- Controller -->
-
-<?php
-class Drugstore extends CI_Controller {
-
-    public function index()
-    {
-        $this->load->view('drugstore\home');
-    }
-}
-
